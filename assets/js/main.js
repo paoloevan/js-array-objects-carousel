6a3ecba3338132d6c45449a600dddd99f75243e7
paoloevan <paoloev1900@gmail.com>
2022-10-21 16:55:30 +0200

    add corrections

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@
 BONUS 1 (opzionale):
 Aggiungere le thumbnails (sottoforma di miniatura) ed al click attivare l’immagine corrispondente.
 BONUS 2  (opzionale):
-Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi) l’immagine attiva dovrà cambiare alla successiva.
+Aggiungere funzionalità di autoplay: dopo un certo periodo di tempo (3 secondi)
+l’immagine attiva dovrà cambiare alla successiva.
 BONUS 3  (opzionale):
 Aggiungere bottoni di start/stop e di inversione del meccanismo di autoplay.
 */
@@ -58,18 +59,23 @@ const btn4El = document.querySelector('.btn_4');
 
 let thisImg = 0;
 
+//funzionalità di autoplay
+setInterval(() => {
+    thisImg++
+}, 1000);
+
+
 //inserisco immagine
 images.forEach((img, i) => {
     //inserisco immagine
     //slidesEl.insertAdjacentHTML('beforeend', `<img class="${i == thisImg ? 'image active' : 'image'}" src="./assets/${img.image}" alt="">`)
     slidesEl.insertAdjacentHTML('beforeend', `
-    <div class="${i == thisImg ? 'slide active' : 'slide'}">
+        <div class="${i == thisImg ? 'slide active' : 'slide'}">
         <img src="./assets/${img.image}" alt="" alt="">
         <h2>${img.title}</h2>
         <p>${img.text}</p>
-    </div>`)
-
-
+        </div>`)
+   
 })
 
 
